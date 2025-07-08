a3c1b6934f40b4962744675ad9b3163c075166ce
Laiba Aslam <aslamlaiba713@gmail.com>
2025-07-08 16:32:41 +0500

    update changes

--- assignment-1/src/app/page.tsx
+++ assignment-1/src/app/page.tsx
@@ -13,35 +13,42 @@ export default function Home() {
 
   const [topic, setTopic] = useState("");
   const [result, setResult] = useState<Quote[]>([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
 
   const handleSubmit = () => {
     const filtered = quotes
       .filter(q => q.topic.toLowerCase() === topic.toLowerCase())
       .slice(0, 3);
     setResult(filtered);
+    setHasSearched(true);
   };
 
   return (
-    <main className="max-w-xl mx-auto p-6 space-y-6">
-      <h1 className="text-2xl font-bold text-center">Quote Generator</h1>
-      <Input
-        className="w-full mx-auto block"
-        placeholder="Enter topic"
-        value={topic}
-        onChange={(e) => setTopic(e.target.value)}
-      />
-      <Button className="mx-auto block w-full flex justify-center" onClick={handleSubmit}>Get Quotes</Button>
+    <div className="min-h-screen flex items-center justify-center bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat">
+      <main className="max-w-xl mx-auto  w-full bg-white/30 backdrop-blur-lg rounded-2xl shadow-lg p-6 space-y-6">
+        <h1 className="text-2xl font-bold text-center">Quote Generator</h1>
+        <Input
+          className="w-full mx-auto block"
+          placeholder="Enter topic"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+        />
+        <Button className="mx-auto block w-full flex justify-center" onClick={handleSubmit}>Get Quotes</Button>
 
-      <div className="space-y-4">
-        {result.length === 0 && <p className="text-sm text-gray-500">No quotes found.</p>}
-        {result.map((quote, index) => (
-          <Card key={index}>
-            <CardContent className="p-4">
-              <p className="text-gray-700 italic">{quote.text}</p>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    </main>
+        <div className="space-y-4">
+          {hasSearched && result.length === 0 && (
+            <p className="text-sm text-gray-500">No quotes found.</p>
+          )}
+          {result.map((quote, index) => (
+            <Card key={index}>
+              <CardContent className="p-4">
+                <p className="text-gray-700 italic">{quote.text}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </main>
+    </div>
   );
 } 
