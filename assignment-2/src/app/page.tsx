f3a837a3f804b0a310d9bde1789af7a7d1b1970e
Laiba Aslam <aslamlaiba713@gmail.com>
2025-07-14 16:35:11 +0500

    update page.tsx

--- assignment-2/src/app/page.tsx
+++ assignment-2/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('/background.jpg')] bg-cover bg-center bg-no-repeat">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[url('/background_sc.png')] bg-cover bg-center bg-no-repeat">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:text-center">
         <h1 className="text-3xl font-bold text-center sm:text-center text-white">Blog Summariser</h1>
         <p className="text-lg text-center sm:text-center text-white">
